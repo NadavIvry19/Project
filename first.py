27c1793c6072692e7884b87d4ee9da8094f35157
NadavIvry19 <124959164+NadavIvry19@users.noreply.github.com>
2024-02-03 04:44:52 +0200

    Update first.py

--- first.py
+++ first.py
@@ -1,11 +1,39 @@
-def generate_table():
-    print("|   x   |   0   |   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |   10  |")
-    print("|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|")
-    
-    for i in range(0, 11):
-        row_values = [i * j for j in range(0, 11)]
-        row_str = f"|   {i}   |" + "   |".join(f"  {value}"[-4:] for value in row_values) + "   |"
-        print(row_str)
+import random
+import time
 
-if __name__ == "__main__":
-    generate_table()
+MAX = 12
+MIN = 3
+OPERATORS = ['+', '-', '*', '%']
+TOTAL = 10
+
+
+def prob():
+    right = random.randint(MIN, MAX)
+    left = random.randint(MIN, MAX)
+    operator = random.choice(OPERATORS)
+
+    expression = str(left) + ' ' + operator + ' ' + str(right)
+    user_answer = eval(expression)
+    return expression, user_answer
+
+
+wrong = 0
+input('press Enter to start!')
+print('--------------------')
+
+start_time = time.time()
+
+for i in range(TOTAL):
+    expr, answer = prob()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ' ' + expr + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print('-----------')
+print('Nice work!, you did it in', total_time, 'seconds!')
+print(f'you gave {wrong} wrong answers!')
